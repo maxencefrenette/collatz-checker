9995b473c3170489a142c932b46feef9b7a1bed9
Maxence Frenette <maxence.frenette@gmail.com>
2022-09-27 22:45:58 -0700

    Computs collatz residues up to 2^32

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let mut n: u32 = 1;
-    let mut residues: Vec<u32> = vec![0];
+    let mut n: u64 = 1;
+    let mut residues: Vec<u64> = vec![0];
 
-    for _ in 0..12 {
+    for _ in 0..32 {
         residues.extend(residues.clone().iter().map(|r| n + r));
         n *= 2;
 
@@ -11,12 +11,12 @@ fn main() {
             .filter(|r| !check_collatz(n, *r))
             .collect();
 
-        println!("Residues mod {}: {:?}", n, residues);
+        println!("Residues mod {}: {}", n, residues.len());
     }
 }
 
 /// Checks if numbers of the form a*k + b satisfy the collatz conjecture for all k
-fn check_collatz(a: u32, b: u32) -> bool {
+fn check_collatz(a: u64, b: u64) -> bool {
     let a_start = a;
     let mut a = a;
     let mut b = b;
